17d404bcf2a6d84144cbee529ff5a97161ebd2ef
Capuccino <chinodesuuu@gmail.com>
2017-11-20 03:32:16 +0000

    change to updater and add OS Detection.

---
+++ src/Monika.Discord/Updater/Updater.cs
@@ -0,0 +1,38 @@
+//Fucking Monikammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+// this on MIT btw uwu
+
+using System;
+using System.Net.Http;
+using Squirrel.Windows;
+
+namespace Monika.Updater
+{
+    public class Program 
+    {
+        public static void Main () 
+        {
+            // check for Operating system first
+            var os = Environment.OSVersion;
+            var platform = os.Platform;
+            switch(platform)
+            {
+                #region Windows Cases
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                //TODO: Squirrel stuff here
+                 break;
+               #endregion
+               
+               #region Unix Updater case
+                case PlatformID.Unix :
+                //TODO: System.Http stuff here
+                break;
+               #endregion
+            }
+            
+        }
+    }
+}

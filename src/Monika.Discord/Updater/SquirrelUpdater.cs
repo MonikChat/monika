b8e9ac25762a4d5efc28f18193234a6b62656163
Capuccino <chinodesuuu@gmail.com>
2017-11-20 01:22:45 +0000

    time to add this shit cos im bored and gya

---
+++ src/Monika.Discord/Updater/SquirrelUpdater.cs
@@ -0,0 +1,17 @@
+// A Hello World! program in C#.
+using System;
+using Squirrel;
+namespace Monika.Updater
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            // variables for Nuget paths
+            // Unix users: please make a symbolic link to /usr/bin for this to work.
+            
+           private string WindowsNugetPath = "C:\Program Files\NuGet\16.0\NuGet.exe";
+           private string UnixNugetPath = "/usr/bin/nuget";
+        }
+    }
+}

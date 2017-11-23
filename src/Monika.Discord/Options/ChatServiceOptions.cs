007ad7a617e7b1a05163c3b22a4423c1884c0f3b
Capuccino <chinodesuuu@gmail.com>
2017-11-23 06:38:56 +0000

    switch to Clara.Minimal

--- src/Monika.Discord/Options/ChatServiceOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Monika.Options
-{
-    public class ChatServiceOptions
-    {
-        public string ApiKey { get; set; }
-        public string ProjectId { get; set; }
-    }
-}
---
+++ src/bot.js
@@ -0,0 +1,47 @@
+/**
+  * @file the main file the makes everything work
+  * @author Capuccino
+  * @author Ovyerus
+  * @copyright Copyright (c) 2017 Capuccino, Ovyerus and the repository contributors.
+  * @license MIT
+  */
+
+// Imports
+const Clara = require(`${__dirname}/lib/Clara`);
+const fs = require('fs');
+const config = require(`${__dirname}/config.json`);
+
+// Globals
+global.mainDir = __dirname;
+global.utils = require(`${__dirname}/lib/modules/utils`);
+global.logger = require(`${__dirname}/lib/modules/Logger`);
+global.Promise = require('bluebird');
+global.got = require('got');
+
+//bot stuff
+const bot = new Clara(config, {
+    seedVoiceConnections: true,
+    maxShards: config.maxShards || 1,
+    latencyThreshold: 420000000,
+    defaultImageFormat: 'webp',
+    defaultImageSize: 512,
+    disableEvents: {
+        TYPING_START: true
+    }
+});
+
+bot.commandsDir = `${__dirname}/cmd`;
+bot.unloadedPath = `${__dirname}/data/unloadedCommands.json`;
+
+//Promise configuration
+Promise.config({
+    warnings: {wForgottenReturn: config.promiseWarnings || false},
+    longStackTraces: config.promiseWarnings || false
+});
+
+if (!fs.existsSync(`${__dirname}/cache`)) fs.mkdirSync(`${__dirname}/cache/`);
+
+require(`${__dirname}/lib/events`)(bot);
+bot.connect();
+
+exports.bot = bot;

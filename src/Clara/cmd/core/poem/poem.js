1545df883348fa59821610519f73976184837095
sr229 <aguilarralph13@gmail.com>
2017-11-28 18:52:00 +0800

    refactor first arg handling

--- src/Clara/cmd/core/poem/poem.js
+++ src/Clara/cmd/core/poem/poem.js
@@ -11,15 +11,7 @@ exports.poem = {
     desc: 'Make a poem!',
     usage: '<yuri | yuri2 | yuri3 | natsuki | monika | sayori> <poem>',
     main(bot, ctx) {
-        let allowedNames = [
-            `${[Ss]}ayori`,
-            `${[Yy]}uri`,
-            `${[Yy]}uri2`,
-            `${[Yy]}uri3`,
-            `${[Nn]}atsuki`,
-            `${[Mm]}onika`,
-        ]
-        if (!typeof ctx.args[1] === allowedNames) {
+        if (!/^(yuri[1-3]?|monika|natsuki|sayori)$i/.test(ctx.args[0])) {
             await ctx.createMessage('Hey, Provide me which kind of writing style I should use. run "help poem" for the acceptable names.');
         } else if (!ctx.args[2]) {
             await ctx.createMessage('Give me a poem to write with.');

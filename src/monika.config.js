43e018cbda01f1267beb93476c2e5b80dc09d4ef
sr229 <aguilarralph13@gmail.com>
2017-11-28 18:21:09 +0800

    redo ecosystem file

--- src/monika.config.js
+++ src/monika.config.js
@@ -4,25 +4,24 @@
  */
 module.exports = {
     apps: [{
-    "name": "Monika.Gateway",
-    "script": "./bot.js",
-    "error_file": "../logs/gateway/err.log",
-    "out_file": "../logs/gateway/out.log",
-    "log_date_format": "YYYY-MM-DD HH:mm Z",
-    "watch": ["./bot.js"],
-    "ignore_watch": ["data", "logs", "cmd", "cache", "node_modules"],
-    "args": ["--color"]
-      },
-      {
-       //TODO: Figure out Monika.PoemService's API. It seems ot be HTTP-based
-      "name": "Monika.PoemService",
-      "interpreter": "/usr/bin/luajit",
-      "script": "./ImageGenerator/main.lua",
-      "error_file": "../logs/PoemService/err.log",
-      "out_file": "../logs/PoemService/out.log",
-      "log_date_format": "YYYY-MM-DD HH:mm Z",
-      "watch": ["./ImageGenerator/main.lua"],
-      "ignore_watch": ["data", "logs", "cmd", "cache", "node_modules"],
-      "args": ["--color"]
-      }]
-}
+        name: 'Monika.Gateway',
+        script: './Clara/bot.js',
+        error_file: '../logs/gateway/err.log',
+        out_file: '../logs/gateway/out.log',
+        log_date_format: 'YYYY-MM-DD HH:mm Z',
+        watch: ['./Clara/bot.js'],
+        ignore_watch: ['data', 'logs', 'cmd', 'cache', 'node_modules'],
+        args: ['--color']
+    },
+    {
+        //TODO: Figure out Monika.PoemService's API. It seems ot be HTTP-based
+        name: 'Monika.PoemService',
+        interpreter: '/usr/bin/luajit',
+        script: './ImageGenerator/main.lua',
+        error_file: '../logs/PoemService/err.log',
+        out_file: '../logs/PoemService/out.log',
+        log_date_format: 'YYYY-MM-DD HH:mm Z',
+        ignore_watch: ['data', 'logs', 'cmd', 'cache', 'node_modules'],
+        args: ['--color']
+    }]
+};

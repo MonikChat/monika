992c52c48ef49889d5309c277896d3a9c4b6ba65
sr229 <aguilarralph13@gmail.com>
2017-11-28 18:19:58 +0800

    add poem command

---
+++ src/Clara/cmd/core/poem/poem.js
@@ -0,0 +1,30 @@
+/**
+ * @file Poem Generator command
+ * @author Capuccino
+ * @author Ovyerus
+ */
+exports.commands = [
+    'poem'
+];
+
+exports.poem = {
+    desc: 'Make a poem!',
+    usage: '<yuri | yuri2 | yuri3 | natsuki | monika | sayori> <poem>',
+    main(bot, ctx) {
+        let allowedNames = [
+            `${[Ss]}ayori`,
+            `${[Yy]}uri`,
+            `${[Yy]}uri2`,
+            `${[Yy]}uri3`,
+            `${[Nn]}atsuki`,
+            `${[Mm]}onika`,
+        ]
+        if (!typeof ctx.args[1] === allowedNames) {
+            await ctx.createMessage('Hey, Provide me which kind of writing style I should use. run "help poem" for the acceptable names.');
+        } else if (!ctx.args[2]) {
+            await ctx.createMessage('Give me a poem to write with.');
+        } else {
+            return new Error('Not implemented yet.');
+        }
+    }
+}

43c54abcef694dca4ea8709c1e90718a95bb8f0d
Capuccino <chinodesuuu@gmail.com>
2018-01-02 07:50:38 +0000

    Puppet is best waifu

--- src/Clara/cmd/core/chatbot/chatbot.js
+++ src/Clara/cmd/core/chatbot/chatbot.js
@@ -5,19 +5,7 @@
  * @author FiniteReality
  */
 
-const dialogflow = require('dialogflow');
-const uuid = require('uuid/v4');
-let client;
-let spPath;
 exports.commands = ['chat'];
-
-exports.init = bot => {
-    client = new dialogflow.SessionsClient();
-    // @FiniteReality requested to use random UUIDs instead
-    if (!bot.config.dialogFlowProjectID || !typeof bot.config.dialogFlowProjectID === 'string') throw new Error('dialogFlowProjectID is not a string');
-    else spPath = client.sessionPath(uuid.uuidv4(), bot.config.dialogFlowProjectID);
-};
-
 
 exports.chat = {
     desc: 'Chat to Monika',
